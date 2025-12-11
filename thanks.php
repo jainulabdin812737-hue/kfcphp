4452cd61cfabeaed22123297869d4852675ea6c9
jainulabdin812737-hue <jainulabdin812737@gmail.com>
2025-12-11 15:01:01 +0530

    Update thanks.php
    ffffffffff

--- thanks.php
+++ thanks.php
@@ -40,4 +40,69 @@
 </div>
 <br>
 <br>
-<?php include('footer.php'); ?>
+
+<?php include('header.php'); ?>
+<style>
+    .alert {
+    margin:auto;
+    color: #000;
+    border-radius: 0;
+    border: 0;
+    font-size: 1.1rem;
+    line-height: 1.5;
+    margin-bottom: 1.875rem;
+    padding: 1.25rem;
+    position: relative;
+    text-align: center;
+}
+.thanksimg{
+    padding:0px;
+    margin:0px auto;
+    width:150px;
+    
+}
+</style>
+<br>
+<br>
+<br>
+<br>
+<div class="container">
+    <div class="row">
+        <div class="alert alert-success">
+			<div class="main_head">
+			    <img scr="img/logo.png" <br="">
+             <div class="hs">Success !</div> <center>Your Application form has been submitted please wait our representative team will contact you soon.<br>
+                <br>Stay Tuned with Us for more Updates.
+                <br>All the Best!
+               !</center>
+               <br>
+					<img class="thanksimg" src="img/tanksimg.png">
+					</div>
+					</div>
+    </div>
+</div>
+<br>
+<br>
+<a href="search.php" class="float1">Check Status</a>
+<style>
+    .float1 {
+    position: fixed;
+    width: 75px;
+    height: 75px;
+    padding-top: 8px;
+    bottom: 20px;
+    right: 40px;
+    background-color: #cd3838;
+    color: #FFF;
+    border-radius: 50px;
+    text-align: center;
+    font-size: 18px;
+    box-shadow: 2px 2px 3px #928a8a;
+    z-index: 100;
+}
+.back-to-top.active {
+    visibility: visible;
+    opacity: 0;
+}
+</style>
+<?php include('footer.php'); ?> 
